e04eef652672b9bb7357d90defdf0c42feddb55d
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2023-01-19 10:06:04 -0500

    updated path on Rclone transfer

--- workflow/submission_scripts/rclone_transfer.sh
+++ workflow/submission_scripts/rclone_transfer.sh
@@ -6,6 +6,6 @@ do
     dir=$(basename "${dir}")
 
     rclone copy --transfers 64 --checkers 64 --copy-links --progress \
-        "./results/${dir}/" "gemini1-2:/mnt/gemini1-6/mteb223_uksr/bag222/brain_discovery/data/raw/${dir}/"
+        "./results/${dir}/" "gemini1-2:/mnt/gemini1-6/mteb223_uksr/PROCESSED_DATA/bag222/brain_discovery/data/raw/${dir}/"
 
 done
