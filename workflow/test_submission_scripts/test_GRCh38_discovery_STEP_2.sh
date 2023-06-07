f5258b683ced242321b0597e1e9b452ffdbc5610
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2023-06-07 09:33:04 -0400

    Added dRNA test submission script

--- workflow/test_submission_scripts/test_GRCh38_discovery_STEP_2.sh
+++ workflow/test_submission_scripts/test_GRCh38_discovery_STEP_2.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 
-nextflow ../main.nf --ont_reads_fq "/scratch/bag222/data/ont_data/large_test_data/*.fastq" \
-    --ref "../../references/Homo_sapiens.GRCh38_ERCC.fa" \
-    --annotation "../../references/Homo_sapiens.GRCh38.107_ERCC.gtf" \
-    --housekeeping "../../references/hg38.HouseKeepingGenes.bed" \
+nextflow ../main.nf --ont_reads_fq "/scratch/bag222/data/ont_data/test_data/*.fastq" \
+    --ref "../../../../cDNA_pipeline/references/Homo_sapiens.GRCh38_ERCC.fa" \
+    --annotation "../../../../cDNA_pipeline/references/Homo_sapiens.GRCh38.107_ERCC.gtf" \
+    --housekeeping "../../../../cDNA_pipeline/references/hg38.HouseKeepingGenes.bed" \
     --cdna_kit "PCS111" \
     --out_dir "./test_discovery/" \
     --is_discovery "True" \
